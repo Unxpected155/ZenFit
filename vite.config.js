00d72d1584f1c15809b4313d3cb3c4d2858db944
JosephGray514 <josephgray514@gmail.com>
2024-01-14 16:17:09 -0600

    Removing connection database from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,26 +3,6 @@ import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
 import mysql from "mysql2";
 
-// Create the connection to the database
-const connection = mysql.createConnection(process.env.DATABASE_URL);
-
-// simple query
-connection.query("show tables", function (err, results, fields) {
-  console.log(results); // results contains rows returned by server
-  console.log(fields); // fields contains extra metadata about results, if available
-});
-
-// Example with placeholders
-connection.query(
-  "select 1 from dual where ? = ?",
-  [1, 1],
-  function (err, results) {
-    console.log(results);
-  }
-);
-
-connection.end();
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
